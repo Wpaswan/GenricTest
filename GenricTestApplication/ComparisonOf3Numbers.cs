06d58b1e4429e05b68651500c4216c4841dd3f08
Wpaswan <dablumcapaswan@gmail.com>
2021-12-04 16:03:39 -0800

    [Dablu, added code for comparison between three numbers]

---
+++ GenricTestApplication/ComparisonOf3Numbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricTestApplication
+{
+    internal class ComparisonOf3Numbers
+    {
+        public static int FindMaxIntNumber(int first, int second, int third)
+        {
+            if ((first.CompareTo(second) >0)&&(first.CompareTo(third)>0))
+            {
+                return first;
+            }
+            else if ((second.CompareTo(first)>0)&&(second.CompareTo(third)>0))
+            {
+                return second;
+            }
+            else if ((third.CompareTo(second)>0)&&(third.CompareTo(first)>0))
+            {
+                return third;
+            }
+            else
+            {
+
+                return default;
+            }
+        }
+    }
+}
--- GenricTestApplication/Program.cs
+++ GenricTestApplication/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Welcome to Genric Test Project!!");
+﻿using GenricTestApplication;
+Console.WriteLine("Enter three numbers:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = ComparisonOf3Numbers.FindMaxIntNumber(a, b, c);
+if (max==0)
+{
+    Console.WriteLine("All three numbers are equal!!!");
+}
+else
+{
+    Console.WriteLine("Maximum among three:"+max);
+}

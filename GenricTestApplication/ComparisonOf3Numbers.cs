31ed1e9d5ddf6918787ba4f043cf64ca7c0d944f
Wpaswan <dablumcapaswan@gmail.com>
2021-12-04 16:28:32 -0800

    [Dablu modified the code to compare three float numbers]

--- GenricTestApplication/ComparisonOf3Numbers.cs
+++ GenricTestApplication/ComparisonOf3Numbers.cs
@@ -28,5 +28,25 @@ namespace GenricTestApplication
                 return default;
             }
         }
+        public static float FindMaxFloatNumber(float first, float second, float third)
+        {
+            if ((first.CompareTo(second) >0)&&(first.CompareTo(third)>0))
+            {
+                return first;
+            }
+            else if ((second.CompareTo(first)>0)&&(second.CompareTo(third)>0))
+            {
+                return second;
+            }
+            else if ((third.CompareTo(second)>0)&&(third.CompareTo(first)>0))
+            {
+                return third;
+            }
+            else
+            {
+
+                return default;
+            }
+        }
     }
 }
--- GenricTestApplication/Program.cs
+++ GenricTestApplication/Program.cs
@@ -1,4 +1,10 @@
 ﻿using GenricTestApplication;
+Console.WriteLine("-----------------------please select any            ----------------------------");
+Console.WriteLine("-----------------------1.Maximum between three integer----------------------------");
+Console.WriteLine("-----------------------2.Maximum between three float__----------------------------");
+int choice=Convert.ToInt32(Console.ReadLine());
+switch (choice) {
+    case 1:
 Console.WriteLine("Enter three numbers:");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
@@ -11,4 +17,25 @@ if (max==0)
 else
 {
     Console.WriteLine("Maximum among three:"+max);
+}
+        break;
+    case 2:
+Console.WriteLine("Enter three float numbers:");
+float firstfloat=Convert.ToSingle(Console.ReadLine());
+
+float secondfloat = Convert.ToSingle(Console.ReadLine());
+float thirdfloat = Convert.ToSingle(Console.ReadLine());
+float maxfloat=ComparisonOf3Numbers.FindMaxFloatNumber(firstfloat, secondfloat, thirdfloat);
+if (maxfloat==0)
+{
+    Console.WriteLine("All three numbers are equal!!!");
+}
+else
+{
+    Console.WriteLine("Maximum among three:"+maxfloat);
+}
+        break;
+        default:
+        Console.WriteLine("Please select a valid one!!");
+        break;
 }
